617a25fc700715908af7ea2d21469d2145a34f0c
Aida Abdikulova <abdikulova@gmail.com>
2019-09-28 05:03:14 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -17,11 +17,11 @@ class Show < ActiveRecord::Base
   end
 
   def self.ratings_sum
-    self.sum("ratings")
+    self.sum("rating")
   end
 
   def self.popular_shows
-    self.where("ratings > ?",5)
+    self.where("rating > ?",5)
   end
 
   def self.shows_by_alphabetical_order
